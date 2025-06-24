a322007f55f0bd837118132045f2deac2ec4e920
orange917 <xiaochengzhi@mail.nwpu.edu.cn>
2025-06-24 11:25:49 +0800

    事务的实现

--- src/transaction/transaction_manager.h
+++ src/transaction/transaction_manager.h
@@ -74,12 +74,15 @@ public:
      * @return {Transaction*} 事务对象的指针
      * @param {txn_id_t} txn_id 事务ID
      */    
-    Transaction* get_transaction(txn_id_t txn_id) {
+     Transaction* get_transaction(txn_id_t txn_id) {
         if(txn_id == INVALID_TXN_ID) return nullptr;
         
         std::unique_lock<std::mutex> lock(latch_);
-        assert(TransactionManager::txn_map.find(txn_id) != TransactionManager::txn_map.end());
-        auto *res = TransactionManager::txn_map[txn_id];
+        auto it = TransactionManager::txn_map.find(txn_id);
+        if (it == TransactionManager::txn_map.end()) {
+            return nullptr;
+        }
+        auto *res = it->second;
         lock.unlock();
         assert(res != nullptr);
         assert(res->get_thread_id() == std::this_thread::get_id());
