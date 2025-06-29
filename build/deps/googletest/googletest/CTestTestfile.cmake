8ddb05cb04cc61f196c2a0967c5094a1d8fe1961
orange917 <xiaochengzhi@mail.nwpu.edu.cn>
2025-06-29 20:42:51 +0800

    删除build文件夹

--- build/deps/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/code/db2025/deps/googletest/googletest
-# Build directory: /home/code/db2025/build/deps/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
